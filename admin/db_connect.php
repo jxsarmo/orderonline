0e22adc821aebc691a120e8ee289572419425fe2
jxsarmo <juxsarmo@gmail.com>
2024-03-01 14:27:35 -0700

    conn

--- admin/db_connect.php
+++ admin/db_connect.php
@@ -1,5 +1,11 @@
 <?php 
 
+$server = 'mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+$user = 'silulw6q1bquo1gc';
+$password = 'qcdzu4wvonapunu4';
+$db_name = 'c7w3q9s4d50778ny'; 
+$port = '3306'; 
+
 /*$conn= new mysqli('localhost','root','root','yummy_bar_new', '3306')or die("Could not connect to mysql".mysqli_error($con));*/
-$conn= new mysqli('mna97msstjnkkp7h.cbetxkdyhwsb.us-east-1.rds.amazonaws.com','silulw6q1bquo1gc','qcdzu4wvonapunu4','c7w3q9s4d50778ny', '3306')or die("Could not connect to mysql".mysqli_error($con));
+$conn= new mysqli($server, $user, $password, $db_name, $port)or die("Could not connect to mysql".mysqli_error($con));
 
